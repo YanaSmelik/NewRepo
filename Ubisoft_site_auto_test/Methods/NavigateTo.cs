9926cdd4e6e2be43bd823b30ac275d487f5eba07
YanaSmelik <39877972+YanaSmelik@users.noreply.github.com>
2020-07-14 11:04:31 +0000

    Update NavigateTo.cs

--- Ubisoft_site_auto_test/Methods/NavigateTo.cs
+++ Ubisoft_site_auto_test/Methods/NavigateTo.cs
@@ -12,10 +12,6 @@ namespace Coursera_auto_test
 
     public static class NavigateTo
     {
-        /*IWebElement languagesDDM = Driver.driver.FindElement(By.CssSelector("#___gatsby > div > div > div > div.styles__Container-sc-1ejj94l-5.nFMFT > div > div.styles__RightPane-sc-1ejj94l-4.hfHosh > div > div > div.select.control.styles__Control-kt7x31-0.hIWHVK > div.select.arrow"));
-
-        var selectElement = new SelectElement();
-
-        public IWebElement LanguagesDDM { get => languagesDDM; set => languagesDDM = value; }*/
+ 
     }
 }
